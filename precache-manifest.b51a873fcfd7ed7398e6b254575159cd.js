34eb59657e8915210077edbea6b37b4de5027090
cristianperca <cristianperca@yahoo.com>
2020-02-18 19:27:03 +0100

    Updates

---
+++ precache-manifest.b51a873fcfd7ed7398e6b254575159cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ce6c5d9bdc58cfea6cc98862e62790",
+    "url": "/cristianperca/chessboard-with-move-validation.git/index.html"
+  },
+  {
+    "revision": "5fe2abcbaf8d6041f262",
+    "url": "/cristianperca/chessboard-with-move-validation.git/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "23a81dd7f3434368384c",
+    "url": "/cristianperca/chessboard-with-move-validation.git/static/js/2.3c758ad9.chunk.js"
+  },
+  {
+    "revision": "96bc018e872f02610f68376ab8fb7ffa",
+    "url": "/cristianperca/chessboard-with-move-validation.git/static/js/2.3c758ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe2abcbaf8d6041f262",
+    "url": "/cristianperca/chessboard-with-move-validation.git/static/js/main.55308aa8.chunk.js"
+  },
+  {
+    "revision": "8c48a3210ae7c91d4257",
+    "url": "/cristianperca/chessboard-with-move-validation.git/static/js/runtime-main.5ab11795.js"
+  }
+]);
